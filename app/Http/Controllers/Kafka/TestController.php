c01ec4758bd1975ffacea654fb75eb6654dff293
Damon <whliujun@hotmail.com>
2018-03-13 12:31:39 +0800

    test kafka

--- app/Http/Controllers/Kafka/TestController.php
+++ app/Http/Controllers/Kafka/TestController.php
@@ -8,7 +8,7 @@ use App\Http\Controllers\Controller;
 
 class TestController extends Controller
 {
-    public function index()
+    public function producer()
     {
         $config = \Kafka\ProducerConfig::getInstance();
         $config->setMetadataRefreshIntervalMs(10000);
@@ -26,6 +26,7 @@ class TestController extends Controller
                 ),
             );
         });
+
         $producer->success(function ($result) {
             var_dump($result);
         });
@@ -33,5 +34,20 @@ class TestController extends Controller
             var_dump($errorCode);
         });
         $producer->send(true);
+    }
+
+    public function consumer()
+    {
+        $config = \Kafka\ConsumerConfig::getInstance();
+        $config->setMetadataRefreshIntervalMs(10000);
+        $config->setMetadataBrokerList('10.13.4.159:9192');
+        $config->setGroupId('test');
+        $config->setBrokerVersion('0.8.2.1');
+        $config->setTopics(array('test'));
+        //$config->setOffsetReset('earliest');
+        $consumer = new \Kafka\Consumer();
+        $consumer->start(function ($topic, $part, $message) {
+            var_dump($message);
+        });
     }
 }

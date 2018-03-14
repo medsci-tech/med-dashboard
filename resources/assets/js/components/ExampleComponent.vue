b16726d5572c77ff904cb14adf71c11b66f4a469
fishyu <ifeinstein@163.com>
2018-03-14 13:50:20 +0800

    add line chart demo

--- resources/assets/js/components/ExampleComponent.vue
+++ resources/assets/js/components/ExampleComponent.vue
@@ -1,23 +1,36 @@
 <template>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card card-default">
-                    <div class="card-header">Example Component</div>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-8">
+        <div class="card card-default">
+          <div class="card-header">demo</div>
 
-                    <div class="card-body">
-                        I'm an example component.
-                    </div>
-                </div>
-            </div>
+          <div class="card-body">
+            <ve-line :data="chartData" />
+          </div>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        mounted() {
-            console.log('Component mounted.')
-        }
-    }
+  import VeLine from 'v-charts/lib/line'
+
+  export default {
+    created () {
+      this.chartData = {
+        columns: ['日期', '销售量'],
+        rows: [
+          {'日期': '1月1日', '销售量': 123},
+          {'日期': '1月2日', '销售量': 1223},
+          {'日期': '1月3日', '销售量': 2123},
+          {'日期': '1月4日', '销售量': 4123},
+          {'日期': '1月5日', '销售量': 3123},
+          {'日期': '1月6日', '销售量': 7123}
+        ]
+      }
+    },
+    components: {VeLine}
+  }
 </script>

0d4557ee1a03a0b553e99a0bbd5eeded484d9639
Damon <whliujun@hotmail.com>
2018-03-26 16:57:31 +0800

    fix

--- app/Http/Controllers/Dashboard/DAUController.php
+++ app/Http/Controllers/Dashboard/DAUController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\DashBoard;
 use App\Http\Requests\DashBoard\DAURequest;
 use App\Http\Controllers\Controller;
 use App\Facades\Helper\Helper;
-use App\Models\MimeLog;
+use App\Models\MimeLoginLog;
 
 /**
  * Class DAUController
@@ -22,7 +22,7 @@ class DAUController extends Controller
         $dates = Helper::getDateFromRange($request->input('start_date'), $request->input('end_date'));
         $countArray = array();
         foreach ($dates as $date) {
-            $countArray[$date] = sizeof(MimeLog::where('action', 'login')->whereBetween('created_at', [date_create($date), date_create(date("Y-m-d", strtotime("+1 day", strtotime($date))))])->distinct('phone')->get());
+            $countArray[$date] = sizeof(MimeLoginLog::where('action', 'login')->whereBetween('created_at', [date_create($date), date_create(date("Y-m-d", strtotime("+1 day", strtotime($date))))])->distinct('phone')->get());
         }
         return response()->json([
             'status' => 1,

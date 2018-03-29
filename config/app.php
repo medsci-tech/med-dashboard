4acf304e77978850dd4e2341ae472c7051492049
Damon <whliujun@hotmail.com>
2018-03-29 16:42:38 +0800

    fix

--- config/app.php
+++ config/app.php
@@ -64,8 +64,7 @@ return [
     | ahead and set this to a sensible default for you out of the box.
     |
     */
-
-    'timezone' => 'UTC',
+    'timezone' => 'PRC',
 
     /*
     |--------------------------------------------------------------------------

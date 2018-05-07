9421ffc2a952f7fbf4181e36388e667f18ca82ea
congkeCSDN <1305709312@qq.com>
2018-05-07 14:09:03 +0800

    修改api地址

--- resources/assets/js/service/api.js
+++ resources/assets/js/service/api.js
@@ -1,7 +1,7 @@
 import fetch from '../config/api'
 // 这里统一配置
-export const pcReg = (start, end) => fetch.post('http://statistics.local.com/api/statistics/mime/register?start_date=' + start + '&end_date=' + end);
-export const pcBro = (start, end) => fetch.post('http://statistics.local.com/api/statistics/mime/dau?start_date='+ start + '&end_date=' + end);
-export const wcReg = (start, end) => fetch.post('http://statistics.local.com/api/statistics/mime/wechat-register?start_date='+ start + '&end_date='+ end);
-export const wcBro = (start, end) => fetch.post('http://statistics.local.com/api/statistics/mime/wechat-dau?start_date='+ start + '&end_date='+ end);
-export const userAuth = (start, end) => fetch.post('http://statistics.local.com/api/statistics/mime/profile?start_date='+ start + '&end_date='+ end )
+export const pcReg = (start, end) => fetch.post('http://statistics.mime.org.cn/api/statistics/mime/register?start_date=' + start + '&end_date=' + end);
+export const pcBro = (start, end) => fetch.post('http://statistics.mime.org.cn/api/statistics/mime/dau?start_date='+ start + '&end_date=' + end);
+export const wcReg = (start, end) => fetch.post('http://statistics.mime.org.cn/api/statistics/mime/wechat-register?start_date='+ start + '&end_date='+ end);
+export const wcBro = (start, end) => fetch.post('http://statistics.mime.org.cn/api/statistics/mime/wechat-dau?start_date='+ start + '&end_date='+ end);
+export const userAuth = (start, end) => fetch.post('http://statistics.mime.org.cn/api/statistics/mime/profile?start_date='+ start + '&end_date='+ end )
